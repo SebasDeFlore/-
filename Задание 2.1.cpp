707fc7d421fa529122f667a62c7f0c0ec2488f05
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2021-10-06 12:00:19 +0300

    Add files via upload

---
+++ Задание 2.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b;
+cout << "Введите a ";  
+cin>> a;
+cout << "Введите b ";  
+cin>> b;
+int c = a + b;
+cout <<"Ответ " << c <<
+endl;
+    return 0;
+}
