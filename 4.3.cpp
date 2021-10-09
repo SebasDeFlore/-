583dbbff17b2101b976f847a67afa1a21ba1e0c4
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2021-10-09 11:08:21 +0300

    Add files via upload

---
+++ 4.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b;
+cout << "Введите число a ";
+cin >> a;
+cout << "Введите число b ";
+cin >> b;
+int s = (a+b)/2;
+cout <<"Среднее арифметическое = " << s <<
+endl;
+
+    return 0;
+}
