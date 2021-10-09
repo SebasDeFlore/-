86dc6b7a2e4e0ff6d223c29d57d4df24460471e2
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2021-10-09 12:09:10 +0300

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -7,10 +7,10 @@ int main()
 { setlocale(0,
 "");
 int d;
-int p = 3.14;
+float p = 3.14;
 cout << "Введите диаметр окружности d ";
 cin >> d;
-int L = p * d;
+float L = p * d;
 cout << "Диаметр окружности = " << L <<
 endl;
     return 0;
