2c1efb25604c142d72c1f8c2b9b4cbe42fb934be
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2021-10-20 11:23:46 +0300

    Update Задание 2.2.cpp

--- Задание 2.2.cpp
+++ Задание 2.2.cpp
@@ -7,19 +7,20 @@ int main()
 { setlocale(0,
 "");
 int a, b;
+printf("a+b должно быть равно 5");
 int c=5;
-cout << "Введите a ";  
+cout << " Введите a ";  
 cin>> a;
 cout << "Введите b ";  
 cin>> b;
 int s = a + b;
 if(s != c) {
-        cout << "Ответ должен быть равен 5" << endl;
+        printf("Не верно");
+        exit;
     }
-if(s == c) {
+else
+    {
         cout << "Всё правильно" << endl;
     }
-cout <<"Ответ " << s <<
-endl;
     return 0;
 }
