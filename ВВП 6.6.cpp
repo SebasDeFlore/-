cd3490a18a014512fb425ce89d5bfa36fe4c73ea
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 02:44:26 +0300

    Create ВВП 6.6.cpp

---
+++ ВВП 6.6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, y; 
+cout << "Введите a ";
+cin >> a;
+
+y=a*a;
+a=y*y;
+y=a*a;
+
+cout <<"a**8 = " << y <<
+endl;
+
+
+    return 0;
+}
