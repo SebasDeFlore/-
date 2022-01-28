5e21b17ac1b49053ae638c1b765443ba92eb17da
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 04:36:06 +0300

    Create ВВП 9.2.cpp

---
+++ ВВП 9.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b, c, d; 
+cout << "День К ";
+cin >> a;
+
+
+b=a/7;
+c=a-b*7;
+
+cout <<" День недели для К = " << c <<
+endl;
+
+    return 0;
+}
