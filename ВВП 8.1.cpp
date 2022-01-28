c7396b0b249c5f01972f5ef570127a8792959f9f
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 03:44:27 +0300

    Create ВВП 8.1.cpp

---
+++ ВВП 8.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b, c; 
+cout << "Введите вес програмы ";
+cin >> a;
+
+b= a/1024;
+
+
+cout <<"Целое число килобайтов = " << b <<
+endl;
+
+
+    return 0;
+}
