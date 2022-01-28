ececb4c2d2351ca937fba0a0d70206e9bfee9215
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 08:19:38 +0300

    Create ВВП 13.1.cpp

---
+++ ВВП 13.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b, c, d, e, f; 
+cout << "Цена за 1 кг ";
+cin >> a;
+
+cout <<"Цена за 0.1 кг = " << a/10 <<
+endl;
+cout <<"Цена за 0.2 кг = " << a/10*2 <<
+endl;
+cout <<"Цена за 0.3 кг = " << a/10*3 <<
+endl;
+cout <<"Цена за 0.4 кг = " << a/10*4 <<
+endl;
+cout <<"Цена за 0.5 кг = " << a/10*5 <<
+endl;
+cout <<"Цена за 0.6 кг = " << a/10*6 <<
+endl;
+cout <<"Цена за 0.7 кг = " << a/10*7 <<
+endl;
+cout <<"Цена за 0.8 кг = " << a/10*8 <<
+endl;
+cout <<"Цена за 0.9 кг = " << a/10*9 <<
+endl;
+cout <<"Цена за 1 кг = " << a <<
+endl;
+    return 0;
+}
+
