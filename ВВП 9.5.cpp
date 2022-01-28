4555d22bb0c61a9af6c81219daaefdab4a471519
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 05:06:26 +0300

    Create ВВП 9.5.cpp

---
+++ ВВП 9.5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b, c, d, e; 
+cout << "Год ";
+cin >> a;
+
+
+b=a/100+1;
+
+cout <<" Столетие = " << b <<
+endl;
+
+    return 0;
+}
