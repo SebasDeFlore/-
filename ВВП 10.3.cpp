89cde76347a6d4f570b9733a41d84887ea62e870
Sebas De Flore <90957311+SebasDeFlore@users.noreply.github.com>
2022-01-28 05:24:39 +0300

    Create ВВП 10.3.cpp

---
+++ ВВП 10.3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace
+std;
+
+int main()
+{ setlocale(0,
+"");
+int a, b, c, d, e; 
+cout << "A ";
+cin >> a;
+
+
+if(a/10>0 and a/10<10) {
+        cout <<" Ответ = " << "Да" <<
+endl;
+    } 
+else    {
+        cout <<" Ответ = " << "Нет" <<
+endl;
+    }
+
+
+
+    return 0;
+}
+
